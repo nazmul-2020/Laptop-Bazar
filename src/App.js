9d7609db3d635e8daed97dddf543df974dac428b
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-03-26 20:16:31 +0600

    add blog

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './component/Header/Header';
 import Bazar from './component/Bazar/Bazar';
+import Blog from './component/Blog/Blog';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Bazar/>
+      <Blog/>
     </div>
   );
 }
---
+++ src/component/Blog/Blog.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Blog.css'
+
+const Blog = () => {
+    return (
+        <div className='blog-container'>
+            <h1>Blog</h1>
+            <div className='blog'>
+                <div className='blog-text'>
+                    <h3>How React Works</h3>
+                    <p>fksljdurowi</p>
+                </div>
+                <div className='blog-text'>
+                    <h3>How React Works</h3>
+                    <p>klgfdskgj</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
